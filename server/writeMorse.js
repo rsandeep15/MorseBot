79166eedd27ef81d3f7c20e5d4fb7176756eeff7
Sandeep Raghunandhan <swasandeep@yahoo.com>
2016-11-12 13:48:31 -0800

    moved set allow header to top

--- server/writeMorse.js
+++ server/writeMorse.js
@@ -3,6 +3,7 @@ var url = require("url");
 var Db = require('mongodb').Db;
 MongoClient = require('mongodb').MongoClient;
 function onRequest(request, response){
+	response.setHeader('Access-Control-Allow-Origin', '*');
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	var mongoURL = "mongodb://morsebot:password@ds161475.mlab.com:61475/qpfallteam3";
 	var queryData = url.parse(request.url, true).query;
@@ -15,7 +16,6 @@ function onRequest(request, response){
 				console.log("Could not insert entry");
 				console.log(entry);
 			}
-			response.setHeader('Access-Control-Allow-Origin', '*');
 			response.end();
 		});
 	});
