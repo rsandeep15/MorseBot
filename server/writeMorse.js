b7b30fd2765d92108bf9a4f6e328f3ba79d64189
Sandeep Raghunandhan <swasandeep@yahoo.com>
2016-11-12 12:30:09 -0800

    Functional writeMorse

--- server/writeMorse.js
+++ server/writeMorse.js
@@ -1,17 +1,21 @@
 var http = require("http");
+var url = require("url");
 var Db = require('mongodb').Db;
 MongoClient = require('mongodb').MongoClient;
 function onRequest(request, response){
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	var mongoURL = "mongodb://morsebot:password@ds161475.mlab.com:61475/qpfallteam3";
+	var queryData = url.parse(request.url, true).query;
 	MongoClient.connect(mongoURL,
 	function(err, db) {
-		db.collection('qpfallteam3').insert(request,
+		var entry = {"text" : queryData.text};
+		db.collection('qpfallteam3').insert(entry,
 		function(err, entry){
 			if (err){
 				console.log("Could not insert entry");
+				console.log(entry);
 			}
-			response.end(); 
+			response.end();
 		});
 	});
 }
