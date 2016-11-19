7e2b773a498696afe82b2a554cc84d8f4a24f7a6
Sandeep Raghunandhan <swasandeep@yahoo.com>
2016-11-19 13:43:36 -0800

    Testing node script to aggregate morse

---
+++ template/js/pi.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+  $.ajax({
+      url: 'http://192.168.2.3:8085?field=morse',
+      crossDomain: true,
+      success: function(result){
+      console.log(result);
+  }, error: function(result) {
+      $("#div1").html('error');
+  }});
+  var distribution = $("#dot-dash").getContext("2d");
+  var pieData = [
+    {
+      value: 20,
+      color: "#878BB7"
+    },
+    {
+      value: 40,
+      color: "#4BCAB5"
+    }
+  ];
+  var pieOptions = {
+    segmentShowStroke : false,
+    animateScale : true
+  }
+  new Chart(distribution).Pie(pieData, pieOptions);
+})
