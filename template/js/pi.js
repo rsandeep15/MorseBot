339e8895440ac81d7a010be8fddac182c33e9c13
Sandeep Raghunandhan <swasandeep@yahoo.com>
2016-11-19 14:03:23 -0800

    Calculation fixes

--- template/js/pi.js
+++ template/js/pi.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
       url: 'http://128.54.235.118:8085?field=morse',
       crossDomain: true,
       success: function(result){
-      console.log(result);
+      console.log("Dots: " + result['.'] + " Dashes: " + result['-']);
 
       var distribution = document.getElementById('dot-dash').getContext("2d");
       var pieData = [
         {
-          value: result['.']/(result['.'] + result['-']),
+          value: Math.floor(100 * (result['.']/(result['.'] + result['-']))),
           color: "#878BB7"
         },
         {
-          value: result['-']/(result['.'] + result['-']),
+          value: Math.floor(100 * (result['-']/(result['.'] + result['-']))),
           color: "#4BCAB5"
         }
       ];
