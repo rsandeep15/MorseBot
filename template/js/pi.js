73649a92aeb3d96bbde17c9185f800c7599726a9
Sandeep Raghunandhan <swasandeep@yahoo.com>
2016-11-19 13:58:03 -0800

    switching to remote ip

--- template/js/pi.js
+++ template/js/pi.js
@@ -1,27 +1,29 @@
 
 $(document).ready(function(){
   $.ajax({
-      url: 'http://192.168.2.3:8085?field=morse',
+      url: 'http://128.54.235.118:8085?field=morse',
       crossDomain: true,
       success: function(result){
       console.log(result);
+
+      var distribution = document.getElementById('dot-dash').getContext("2d");
+      var pieData = [
+        {
+          value: result['.']/(result['.'] + result['-']),
+          color: "#878BB7"
+        },
+        {
+          value: result['-']/(result['.'] + result['-']),
+          color: "#4BCAB5"
+        }
+      ];
+      var pieOptions = {
+        segmentShowStroke : false,
+        animateScale : true
+      }
+      new Chart(distribution).Pie(pieData, pieOptions);
   }, error: function(result) {
       $("#div1").html('error');
   }});
-  var distribution = document.getElementById('dot-dash').getContext("2d");
-  var pieData = [
-    {
-      value: 20,
-      color: "#878BB7"
-    },
-    {
-      value: 40,
-      color: "#4BCAB5"
-    }
-  ];
-  var pieOptions = {
-    segmentShowStroke : false,
-    animateScale : true
-  }
-  new Chart(distribution).Pie(pieData, pieOptions);
+
 })
